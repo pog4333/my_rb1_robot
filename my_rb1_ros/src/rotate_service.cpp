6abeb62c7bff7ee7607c6e9a36e4920551f6d758
ori <origranot@gmail.com>
2024-10-25 23:06:14 +0000

    fixed speed and accurcy

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -5,6 +5,7 @@
 #include "ros/rate.h"
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
+#include "ros/subscriber.h"
 #include "tf/LinearMath/Matrix3x3.h"
 #include "tf/LinearMath/Quaternion.h"
 #include "tf/transform_datatypes.h"
@@ -23,20 +24,20 @@ public:
 
     bool result = false;
     double current_yaw;
-    // double degree;
     double start_yaw;
     double converte_degrees;
     double target;
     double x, y, z, w;
     double roll, pitch, yaw;
-
-    // double* ptr;
+    double pi = M_PI;
+    float az;
 
     MyClass(){
-        pub =nh.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+        pub =nh.advertise<geometry_msgs::Twist>("cmd_vel",5);
         my_service = nh.advertiseService("/rotate_robot", &MyClass::my_callback,this);
-        sub = nh.subscribe("odom", 10000, &MyClass::odom_callback,this);
+        sub = nh.subscribe("odom", 5, &MyClass::odom_callback,this);
     }
+    
 
     void odom_callback(const nav_msgs::Odometry::ConstPtr& msg){
     x = msg->pose.pose.orientation.x;
@@ -46,55 +47,44 @@ public:
     tf::Quaternion quat(x, y, z, w);
     tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
     current_yaw = yaw;
-    // ROS_INFO("%f", current_yaw);
+    if (current_yaw < 0){
+      current_yaw = current_yaw + 2*(pi);}
     }
 
     bool my_callback(my_rb1_ros::Rotate::Request &req,
                     my_rb1_ros::Rotate::Response &res
                     ) {
                 
-    ROS_INFO("Service Requested \n Request Data==> rotate_degrees= %d", req.degrees);
-        ros::Rate rate(10);
+    ROS_INFO("Service Requested \n rotate_degrees= %d", req.degrees);
+        ros::Rate rate(100);
         start_yaw = current_yaw;
+        if (start_yaw < 0){
+          start_yaw = start_yaw + 2*(pi);}
         result = false;
 
-    // degree = req.degrees;
-    // rotate_degre(degree);
+
         while (result == false){
-        // ROS_INFO("start while");
-        converte_degrees = req.degrees*3.14159/180;
-        
-        if (start_yaw < 0 && (start_yaw + converte_degrees) < 0){
-            if(req.degrees > 0){
-              target = start_yaw + std::fabs(converte_degrees);}
-            else {
-              target = converte_degrees + start_yaw;
-            }  
+        converte_degrees = req.degrees*pi/180;
+        target = start_yaw + converte_degrees;
+        if(target > 2*(pi)){
+          target = target - (2*pi);
         }
-        else{
-         target = converte_degrees + start_yaw; 
-        }
-        if (std::fabs(target) > 3.1415) {
-            if (target > 0) {
-            target = target - (3.1415*2);
-            }
-            else{
-            target = target + (3.1415*2);
-            }
-            
-        }
-            
-            while (std::fabs(current_yaw - target) > 0.006) {
-            if (req.degrees < 0){
+
+        if (target < 0){
+          target = target + 2*(pi);}
+
+            while (std::fabs(target - current_yaw) > 0.0005) {
+            az = std::fabs(target - current_yaw)*2 + 0.01;
+            if (az > 0.7){
+              az=0.7;}
+            // ROS_INFO(" speed = %f ", az);
+              
             // ROS_INFO("start orientation = %f\n current orientation = %f target = %f" , start_yaw, current_yaw, target);
-            twist.angular.z = -0.1;
-            pub.publish(twist);            
-            }
-            else{
-            // ROS_INFO("start orientation = %f\n current orientation = %f target = %f", start_yaw, current_yaw, target);
-            twist.angular.z = 0.1;
-            pub.publish(twist);         
-            }
+        if (req.degrees < 0){
+        az = az*-1; 
+        }
+            twist.angular.z = az;
+            pub.publish(twist);             
             ros::spinOnce();
             rate.sleep();   
             }
@@ -103,7 +93,7 @@ public:
             pub.publish(twist);
             result = true;
             ROS_INFO("Service Compleated ");
-            res.result = "completed";
+            res.result = "Service/rotate_robot: succeeded,  " + std::to_string(req.degrees) + " degrees" ;
             }
         return true;
         }};
